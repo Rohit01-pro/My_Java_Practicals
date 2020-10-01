adf26ecb35ed8bfdaf0fb42cb0e3dd9053103ef4
abhishekb13402 <72155603+abhishekb13402@users.noreply.github.com>
2020-10-01 14:57:45 +0530

    add comments

--- Practical 1/Question2.java
+++ Practical 1/Question2.java
@@ -4,18 +4,18 @@ public class Question2 {
     static Scanner input = new Scanner(System.in);
 
     static double circumCircle(double radius) {
-        return 2 * Math.PI * radius;
+        return 2 * Math.PI * radius; //2*3.14*10=62.8
     }
 
     static double areaCircle(double radius) {
-        return Math.PI * (radius * radius);
+        return Math.PI * (radius * radius); //3.14*10*10=314
     }
 
     public static void main(String args[]) {
         System.out.print("Enter Radius of Circle: ");
         double radius = input.nextDouble();
-        System.out.println("\nRadius of Circle: " + radius);
-        System.out.println("\nCircumference of Circle: " + circumCircle(radius));
-        System.out.println("\nArea of Circle: " + areaCircle(radius));
+        System.out.println("\nRadius of Circle: " + radius); //10
+        System.out.println("\nCircumference of Circle: " + circumCircle(radius));  //62.8
+        System.out.println("\nArea of Circle: " + areaCircle(radius)); //314
     }
 }
